c477450bb751c03ad1ebf2d149ee1c102984c535
Miti-jan <46924645+Miti-jan@users.noreply.github.com>
2019-06-17 11:56:56 +0430

    UI changes

--- EventHandlingApp/EventHandlingApp/App_Start/BundleConfig.cs
+++ EventHandlingApp/EventHandlingApp/App_Start/BundleConfig.cs
@@ -20,11 +20,13 @@ namespace EventHandlingApp
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap-rtl.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                "~/Content/Site.css",
+                      "~/Content/bootstrap-rtl.css",
+                      "~/Content/bootstrap-theme-rtl.css"
+                      ));
         }
     }
 }
--- EventHandlingApp/EventHandlingApp/App_Start/RouteConfig.cs
+++ EventHandlingApp/EventHandlingApp/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace EventHandlingApp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "EventHandlingApp.Controllers" }
             );
         }
     }
